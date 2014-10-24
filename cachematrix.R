49d5013e3443d5074308863d09bd9c6a6aba1300
weijiang <weijiang@adobe.com>
2014-10-25 02:51:01 +0800

    'update'

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,28 @@
 ## Two functions for R Programming assignment 2
+## 
+## Usage:
+## ------------------------------------
+## > source("cachematrix.R")
+## > c <- rbind(c(1,-1/4),c(-1/4,1))
+## > x <- makeCacheMatrix(c)
+## > x$makeInverseMatrix()
+## > cacheSolve(x)
+## ------------------------------------
 
 ## This function creates a special "matrix" that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
 
+	makeInverseMatrix <- function() {
+		m <<- solve(x)
+	}
+
+	getInverseMatrix <- function() {
+		m
+	}
+
+	list(makeInverseMatrix = makeInverseMatrix, getInverseMatrix = getInverseMatrix)
 }
 
 
@@ -12,5 +31,12 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then the cacheSolve should receive the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+	m <- x$getInverseMatrix()
+	if (!is.null(m)) {
+		message("getting cached data")
+		return (m)
+	}
+	data <- x$makeInverseMatrix()
+	data
 }
